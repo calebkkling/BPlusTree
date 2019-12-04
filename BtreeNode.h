550bb8c74a4a83bb552653683f7296c7f2862368
calebkkling <calebkkling@gmail.com>
2019-12-04 12:10:20 -0600

    Add files via upload

--- BtreeNode.h
+++ BtreeNode.h
@@ -17,7 +17,6 @@
  * -initial value to -1 is unnecessary, a node with no parents(disconnected node) is not yet have value stored
  */
 
-
 #ifndef B_TREE_BTREENODE_H
 #define B_TREE_BTREENODE_H
 
@@ -29,157 +28,178 @@
 
 using namespace std;
 
-
-
-
-class Bptree {
+class Bptree
+{
 private:
-
     //degree of the tree
     int order;
 
-
-
-    struct node {
-        node* parent;
+    struct node
+    {
+        node *parent;
         vector<int> key;
         bool isleaf;
         int value;
-        vector<node*> children;
+        vector<node *> children;
 
-        node() {
+        node()
+        {
             parent = nullptr;
             isleaf = true;
             vector<int> key[7];
-            vector<node*> children[8];
+            vector<node *> children[7];
             value = -1; //value initialized to -1 to show that nothing is stored in the node
             // (Like with the visualization, only positive numbers can be added to the tree)
         }
 
-//        node(node* p, vector<int> k, const vector<node*> c) {
-//            parent = p;
-//            if (!k.empty()) {
-//                for (int i = 0; i < order - 1; i++) {
-//                    key.push_back(k[i]);
-//                }
-//            }
-//            isleaf = false;
-//            for (auto i : c) {
-//                children.push_back(i);
-//            }
-//            value = -1;
-//        }
+        //        node(node* p, vector<int> k, const vector<node*> c) {
+        //            parent = p;
+        //            if (!k.empty()) {
+        //                for (int i = 0; i < order - 1; i++) {
+        //                    key.push_back(k[i]);
+        //                }
+        //            }
+        //            isleaf = false;
+        //            for (auto i : c) {
+        //                children.push_back(i);
+        //            }
+        //            value = -1;
+        //        }
 
-        node(node* p,int k, bool l) {
+        node(node *p, int k, bool l)
+        {
             parent = p;
             key.push_back(k);
             isleaf = true;
-//            value = -1;
+            //            value = -1;
         }
 
         ~node(){};
     };
 
-    node* root = nullptr;
+    node *root = nullptr;
 
     //element that last visited
-    node* last_visited = nullptr;
+    node *last_visited = nullptr;
 
 public:
-
-
-    Bptree(int o){
+    Bptree(int o)
+    {
         root = nullptr;
         order = o;
-
     }
 
-    ~Bptree(){ //not implemented
+    ~Bptree()
+    { //not implemented
+        if(hasChildren(root)){
 
+        }
     }
 
-    node* search(int x) {
-        node* v = root;
+    node *search(int x)
+    {
+        node *v = root;
         int i;
-        while (v) {
-            for (i = 0; i < v->key.size() && x >= v->key[i] ; i++){
-                if (x == v->key[i] && v->isleaf) {
+        while (v)
+        {
+            for (i = 0; i < v->key.size() && x > v->key[i]; i++)
+            {
+                if (x == v->key[i] && v->isleaf)
+                {
                     last_visited = v;
                     return v;
                 }
             }
-
             last_visited = v;
-            if(hasChildren(v)){
+            if (hasChildren(v))
+            {
                 v = v->children[i];
-            }else{
+            }
+            else
+            {
                 return nullptr;
             }
 
             i = 0;
-
-
         }
 
         return nullptr;
-
     }
 
+    node *merge(node *, node *);
 
-
-    node* merge(node*, node*);
-
-    size_t findPos(vector<int> arr,int comp) {
+    size_t findPos(vector<int> arr, int comp)
+    {
         size_t i;
-        for (i = 0; i < arr.size() && comp > arr[i]; i++) {}
+        for (i = 0; i < arr.size() && comp > arr[i]; i++)
+        {
+        }
         return i;
     }
 
-    bool isFull(node* n) {
+    bool isFull(node *n)
+    {
         return n->key.size() == this->order;
     }
 
-    bool hasChildren(node* n) {
-        return n->children.size()!= 0;
+    bool hasChildren(node *n)
+    {
+        return !n->children.empty();
     }
 
     template <typename T>
-    void shiftRight(vector<T> v,size_t pos){
-//        T a;
-//        v.push_back(a);
-        for(size_t i = v.size(); i > pos; i--){
-            v.insert(v.begin()+i,v[i-1]);
+    void shiftRight(vector<T> v, size_t pos)
+    {
+        T a();
+        v.push_back(a);
+        for (size_t i = v.size(); i > pos; i--)
+        {
+            swap(v[i], v[i - 1]);
         }
     }
 
-
-
     // haven't tested yet
-    void solveOverFlow(node* ptr) {
-        while (ptr && ptr->key.size() >= order) {
+    void solveOverFlow(node *ptr)
+    {
+        while (ptr && ptr->key.size() >= order)
+        {
             size_t pivot = ptr->key.size() / 2;
 
-            if(!ptr->isleaf){
+            if (!ptr->isleaf)
+            {
                 //create a right children
-                node* rc = new node();
+                node *rc = new node();
 
+                //                if(!hasChildren(root)){
+                //                    size_t i = pivot+1;
+                //                }
 
+                //right part after the split
+                for (size_t i = pivot; i < ptr->key.size(); i++)
+                {
+                    rc->key.push_back(ptr->key[i]);
+                }
 
+                ptr->key.erase(ptr->key.begin() + pivot, ptr->key.end());
 
-                if(root == ptr && !hasChildren(root)){
-                    for (int i = pivot; i < ptr->key.size(); i++) {
-                        rc->key.push_back(ptr->key[i]);
+                if (hasChildren(ptr))
+                {
+                    for (size_t i = pivot; i < ptr->children.size(); i++)
+                    {
+                        rc->children.push_back(ptr->children[i]);
                     }
 
-                    ptr->key.erase(ptr->key.begin() + pivot, ptr->key.end());
+                    ptr->children.erase(ptr->children.begin() + pivot, ptr->children.end());
+                }
 
-                    node* temp = new node();
+                if (root == ptr)
+                {
+                    node *temp = new node();
                     temp->isleaf = false;
                     rc->parent = temp;
 
                     //new root has the key that suppose to be pushed up
                     temp->key.push_back(rc->key[0]);
-
 
                     //store the children in new root
                     temp->children.push_back(ptr);
@@ -187,114 +207,65 @@ public:
 
                     this->root = temp;
                     ptr->parent = root;
-                    ptr->isleaf = true;
-
-
-                }else{
-                    int pushed = ptr->key[pivot];
-                    for (int i = pivot + 1; i < ptr->key.size(); i++) {
-                        rc->key.push_back(ptr->key[i]);
-                    }
-
-                    ptr->key.erase(ptr->key.begin() + pivot, ptr->key.end());
-
-                    if (hasChildren(ptr)) {
-                        for (size_t i = pivot + 1,j = 0; i < ptr->children.size(); i++,j++) {
-                            rc->children.push_back(ptr->children[i]);
-                            rc->children[j]->parent = rc;
-                        }
-
-
-                        ptr->children.erase(ptr->children.begin() + pivot+1, ptr->children.end());
-                    }
-
-                    rc->isleaf = false;
-                    ptr->isleaf = false;
-
-                    if (root == ptr) {
-
-                        node* temp = new node();
-                        temp->isleaf = false;
-                        rc->parent = temp;
-
-                        temp->key.push_back(pushed);
-                        //store the children in new root
-                        temp->children.push_back(ptr);
-                        temp->children.push_back(rc);
-                        this->root = temp;
-                        ptr->parent = root;
-
-                    }
-
-                    else {
-                        size_t pos = findPos(ptr->parent->key, ptr->key[pivot]);
-                        shiftRight(ptr->parent->key, pos);
-
-                        ptr->parent->key.insert(ptr->parent->key.begin()+pos,pushed);
-                        shiftRight(ptr->parent->children, pos + 1);
-
-                        //ptr->parent->children.insert(ptr->parent->children.begin()+pos,ptr);
-                        ptr->parent->children.insert(ptr->parent->children.begin()+pos+1,rc);
-                        rc->parent = ptr->parent;
-                    }
-
-
-
                 }
+                else
+                {
+                    size_t pos = findPos(ptr->parent->key, ptr->key[pivot]);
+                    shiftRight(ptr->parent->key, pos);
 
+                    ptr->parent->key[pos] = ptr->key[pivot];
+                    shiftRight(ptr->parent->children, pos + 1);
 
-
-            }else{
+                    ptr->parent->children[pos] = ptr;
+                    ptr->parent->children[pos + 1] = rc;
+                    rc->parent = ptr->parent;
+                }
+            }
+            else
+            {
 
                 size_t pos = findPos(ptr->parent->key, ptr->key[pivot]);
 
-                shiftRight(ptr->parent->key,pos);
-                ptr->parent->key.insert(ptr->parent->key.begin()+pos,ptr->key[pivot]);
+                shiftRight(ptr->parent->key, pos);
+                ptr->parent->key[pos] = ptr->key[pivot];
 
-                if(pos == 0){
-                    node* left = new node();
-                    //left->isleaf = true;
-                    for(size_t i = 0; i < pivot; i++){
+                if (pos == 0)
+                {
+                    node *left = new node();
+                    left->isleaf = false;
+                    for (size_t i = 0; i < pivot; i++)
+                    {
                         left->key.push_back(ptr->key[i]);
                     }
                     left->parent = ptr->parent;
-                    size_t i = pivot,j = 0,size = ptr->key.size();
-                    for(i,j; i < size; i++,j++){
-						swap(ptr->key[j], ptr->key[i]);
+                    size_t i = pivot, j = 0;
+                    for (i, j; i < ptr->key.size(); i++, j++)
+                    {
+                        ptr->key[j] = ptr->key[i];
                     }
-                    ptr->key.erase(ptr->key.begin()+j,ptr->key.end());
+                    ptr->key.erase(ptr->key.begin() + j, ptr->key.end());
 
-
-                    shiftRight(ptr->parent->children,0);
-					ptr->parent->children.insert(ptr->parent->children.begin(), left);
-                    left->parent = ptr->parent;
-
-                }else{
-                    node* right = new node();
-                    //right->isleaf = true;
-                    for(size_t i = pivot; i < ptr->key.size(); i++){
-                        right->key.push_back(ptr->key[i]);
-
-                    }
-                    ptr->key.erase(ptr->key.begin()+pivot,ptr->key.end());
-
-
-                    shiftRight(ptr->parent->children,pos+1);
-                    ptr->parent->children.insert(ptr->parent->children.begin()+pos+1,right);
-                    right->parent = ptr->parent;
-
-
-
+                    shiftRight(ptr->parent->children, 0);
+                    ptr->parent->children[0] = left;
                 }
+                else
+                {
+                    node *right = new node();
+                    right->isleaf = false;
+                    for (size_t i = pivot; i < ptr->key.size(); i++)
+                    {
+                        right->key.push_back(ptr->key[i]);
+                    }
+                    ptr->key.erase(ptr->key.begin() + pivot, ptr->key.end());
 
+                    shiftRight(ptr->parent->children, pos + 1);
+                    ptr->parent->children[pos + 1] = right;
+                }
             }
 
             ptr = ptr->parent;
-
-
         }
     }
-
 
     //insert success or not, not tested as well
     bool insert(int item) {
@@ -321,27 +292,10 @@ public:
             int i = findPos(last_visited->key, item);
 
 
-//            if(last_visited->isleaf){
-            //make room for store item at position
-//                last_visited->key.push_back(0);
-//                last_visited->children.push_back(nullptr);
+
             shiftRight(last_visited->key,i);
-//            shiftRight(last_visited->children,i);
-//                rotate(last_visited->children.rbegin(),last_visited->children.rbegin()+i,last_visited->children.rend());
 
-            //store the item
             last_visited->key.insert(last_visited->key.begin()+i,item);
-
-//            last_visited->children[i]->key.push_back(item);
-//            last_visited->children[i]->value = item;
-//            last_visited->children[i]->isleaf = true;
-//            last_visited = last_visited->children[i];
-//            }else{
-//                last_visited = last_visited->children[i];
-//                shiftRight(last_visited->key,i);
-//                last_visited->key.insert(last_visited->key.begin()+i,item);
-//
-//            }
 
 
         }
@@ -349,56 +303,69 @@ public:
         solveOverFlow(last_visited);
     }
 
-    void remove(int);
+    void remove(int val){ //INCOMPLETE
+        if(search(val)){  //if value is not found in the Tree, don't do anything
+            node *v = root;
+            bool deleted = false;
+            int i;
+            //find the node and remove node
+            //using same method to get to node as search
+            while (v)
+            {
+                for (i = 0; i < v->key.size() && val > v->key[i]; i++)
+                {
+                    if (val == v->key[i] && v->isleaf)
+                    {
 
-    void levelOrder(ostream& os){
-        queue<node *> queue1, queue2;
-        if(!root){
-            os << "NULL" << endl;
-            return;
-        }
-        queue1.push(root);
-        while(!queue1.empty() || !queue2.empty()){
-            while(!queue1.empty()){
-                if(hasChildren(queue1.front())){
-                    for (auto i : queue1.front()->children) {
-                        queue2.push(i);
+                        v = nullptr;
+                        deleted = true;
+
+                        //Check if node needs to replace the deleted one
+                        if(last_visited == v){
+                            if(hasChildren(last_visited)){
+                                last_visited->key[i] = last_visited->children[i]->key[i];
+                            }
+                        }
+
+                        //Shift a lot of stuff
+
+                        break; //exits for loop once node is deleted
+
                     }
                 }
-                for(int i = 0; i<queue1.front()->key.size(); i++){
-                    if(!queue1.front()->isleaf && queue1.front()!=root){
-                        os << "K: ";
-                    }
-                    os << queue1.front()->key[i] << " ";
+                if(deleted){
+                    break;  //exits while loop after node is deleted
                 }
-                queue1.pop();
-                os << "   ";
+                last_visited = v;
+                if (hasChildren(v))
+                {
+                    v = v->children[i];
+                }
+
+                i = 0;
             }
-            os << endl;
-            while(!queue2.empty()){
-                if(hasChildren(queue2.front())){
-                    for (auto i : queue2.front()->children) {
-                        queue1.push(i);
-                    }
-                }
-                for(int i = 0; i<queue2.front()->key.size(); i++){
-                    if(!queue2.front()->isleaf && queue2.front()!=root){
-                        os << "K: ";
-                    }
-                    os << queue2.front()->key[i] << " ";
-                }
-                queue2.pop();
-                os << "   ";
-            }
-            os << endl;
+
         }
-        os << endl;
     }
 
-
-
-
-
+    void levelOrder(ostream &os)
+    {
+        queue<node *> traversalQueue;
+        traversalQueue.push(root);
+        while (!traversalQueue.empty())
+        {
+            node *current = traversalQueue.front();
+            traversalQueue.pop();
+            for (auto i : current->key)
+            {
+                os << current->key[i] << " ";
+            }
+            for (size_t j = 0; j < current->children.size(); j++)
+            {
+                traversalQueue.push(current->children[j]);
+            }
+        }
+    }
 };
 
 #endif //B_TREE_BTREENODE_H
